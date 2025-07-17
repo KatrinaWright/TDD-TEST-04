abc4e0fc8637ce56a53aaa23109e54a386da6400
alyssa-dannielle <103299483+alyssa-dannielle@users.noreply.github.com>
2025-07-17 12:35:40 -0400

    UPDATE function_1 to be T or F

--- AlyssaTest1.py
+++ AlyssaTest1.py
@@ -5,7 +5,10 @@ def hello_world():
     return "Hello!"
 
 def function_1(data):
-    return 1
+    if data == 1:
+      return True
+    else:
+      return False
 
 def main():
     print("Function 1: ", function_1(1))
