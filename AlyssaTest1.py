599009fda8d8b7647a4a2a7aa78f4631814efd2b
alyssa-dannielle <103299483+alyssa-dannielle@users.noreply.github.com>
2025-07-17 12:38:53 -0400

    ADD if else to function_1

--- AlyssaTest1.py
+++ AlyssaTest1.py
@@ -5,7 +5,10 @@ def hello_world():
     return "Hello!"
 
 def function_1(data):
-    return "6"
+    if data == 1:
+      return True
+    else:
+      return False
 
 def main():
     print("Function 1: ", function_1(1))
